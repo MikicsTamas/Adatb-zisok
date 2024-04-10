b8b7ccc1c95463a1a733750126b22d9ea9bf15db
MikicsTamas <160734744+MikicsTamas@users.noreply.github.com>
2024-04-10 13:10:24 +0200

    gyak7

---
+++ MS SQL.2.sql
@@ -0,0 +1,18 @@
+/*
+3. Készítsen skalár értékű függvényt UDFFerohely néven, amely visszaadja, hogy a paraméterként megkapott foglalás azonosítóhoz hány férőhelyes szoba tartozik!
+a. Teszteljük a függvény működését!
+*/
+
+CREATE OR ALTER FUNCTION UDFFerohely
+(
+	@fazon int
+)
+RETURNS int /*a típust adja vissza*/
+AS 
+BEGIN
+	DECLARE @ferohely int
+    SELECT @ferohely = sz.FEROHELY
+    FROM Foglalas f JOIN Szoba sz ON f.SZOBA_FK = sz.SZOBA_ID
+    WHERE f.FOGLALAS_PK = @fazon
+    RETURN @ferohely
+END
